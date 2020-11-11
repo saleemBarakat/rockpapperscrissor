d6a0680d1edf04471a0e9ec2c3bd2c167540234b
saleemBarakat <saleembarakat00@gmail.com>
2020-11-11 23:21:42 +0100

    almost finished, I have added all the needed functions for the game to work

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
 //variabler för de funktioner jag behöver
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("comuter-score");
-const scoreBoad_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result p");
-const rock_div = document.getElementById("r");
-const papper_div = document.getElementById("p");
-const scissor_div = document.getElementById("s");
+const userScorespan = document.getElementById("user-score");
+const computerScorespan = document.getElementById("computer-score");
+const scoreBoard = document.querySelector(".score-board");
+const result = document.querySelector(".result p");
+const rock = document.getElementById("r");
+const papper = document.getElementById("p");
+const scissor = document.getElementById("s");
 //funktion för dator att göra ett random val
 function getComputerChoise() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-//funktion för att användaren att göra ett val
 
+//funktion för att omvandla en viss bokstav till ett ord att vissa istället för endast en bokstav
+function convertToWord(letter) {
+    if (letter === "r")
+        return "Sten";
+    if (letter === "p")
+        return "Påse";
+    if (letter === "s")
+        return "Sax";
+}
+
+//funktion för att användaren att göra ett val
 function win(userChoice, ComputerChoise) {
     userScore++;
-    userScore_span.innerHTML = userScore;
-    result_div.innerHTML = userChoice + " beats " + ComputerChoise + " you win! ";
+    userScorespan.innerHTML = userScore;
+    computerScorespan.innerHTML = computerScore;
+    result.innerHTML = convertToWord(userChoice) + " vinner över " + convertToWord(ComputerChoise);
+    document.getElementById(userChoice).classList.add("green-glow");
+    setTimeout(function () { document.getElementById(userChoice).classList.remove('green-glow') }, 1000);
 }
-function lose() {
-
+function lose(userChoice, ComputerChoise) {
+    computerScore++;
+    userScorespan.innerHTML = userScore;
+    computerScorespan.innerHTML = computerScore;
+    result.innerHTML = convertToWord(userChoice) + " förlorar mot " + convertToWord(ComputerChoise);
+    document.getElementById(userChoice).classList.add("red-glow");
+    setTimeout(function () { document.getElementById(userChoice).classList.remove('red-glow') }, 1000);
 }
-function draw() {
-
+function draw(userChoice, ComputerChoise) {
+    result.innerHTML = convertToWord(userChoice) + " mot " + convertToWord(ComputerChoise) + " blir lika och ingen vinnner";
+    document.getElementById(userChoice).classList.add("gray-glow");
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('gray-glow') }, 1000);
 }
 
 function game(userChoice) {
@@ -53,15 +73,15 @@ function game(userChoice) {
 //function för att kunna klicka och göra ett val
 function main() {
 
-    rock_div.addEventListener('click', function () {
+    rock.addEventListener('click', function () {
         game("r");
     });
 
-    papper_div.addEventListener('click', function () {
+    papper.addEventListener('click', function () {
         game("p");
     });
 
-    scissor_div.addEventListener('click', function () {
+    scissor.addEventListener('click', function () {
         game("s");
     });
 }
